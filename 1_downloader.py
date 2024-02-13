9654974f36a224fd79a1230dc4c82184d725b7bd
MyNameIsTrez <welfje@gmail.com>
2024-02-13 22:13:47 +0100

    Update 1_downloader.py

--- 1_downloader.py
+++ 1_downloader.py
@@ -1,4 +1,6 @@
 import json
+import os
+import shutil
 from pathlib import Path
 
 import requests
@@ -8,21 +10,31 @@ def download_mods():
     with open("mods.json", "rb") as f:
         mods = json.load(f)
 
-    for mod in mods:
-        if is_pre4_0(mod):
+    for index, mod in enumerate(mods):
+        if is_desired_version(mod):
+            # -1 because of .placeholder file
+            print(f"{index + 1}/{len(mods) - 1}")
+
             url = mod["modfile"]["download"]["binary_url"]
 
             mod_id_directory_path = Path("1_downloads") / str(mod["id"])
 
             mod_id_directory_path.mkdir(exist_ok=True)
 
-            mod_file = requests.get(url)
-            with open(mod_id_directory_path / mod["modfile"]["filename"], "wb") as f:
+            zip_path = mod_id_directory_path / mod["modfile"]["filename"]
+
+            # TODO: There must be a way to unpack the zip directly,
+            # without first writing the zip to disk
+            with open(zip_path, "wb") as f:
+                mod_file = requests.get(url)
                 f.write(mod_file.content)
 
+            shutil.unpack_archive(zip_path, mod_id_directory_path)
+            zip_path.unlink()
 
-def is_pre4_0(mod):
-    return any(tag["name"] == "Pre-Release 4.0" for tag in mod["tags"])
+
+def is_desired_version(mod):
+    return any(tag["name"] == "Pre-Release 5.0" for tag in mod["tags"])
 
 
 if __name__ == "__main__":
