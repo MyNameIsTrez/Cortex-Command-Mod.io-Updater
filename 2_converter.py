5e2c26716c81da55accd4b665e28abe421d6dbcb
MyNameIsTrez <welfje@gmail.com>
2024-02-13 22:13:53 +0100

    Update 2_converter.py

--- 2_converter.py
+++ 2_converter.py
@@ -1,22 +1,26 @@
 import os
+import shutil
+import subprocess
 from pathlib import Path
-
-from cortex_command_mod_converter_engine import convert
 
 
 def convert_mods():
-    # TODO: Try to remove .resolve() here
+    converter_dir_path = Path("I:/Programming/Cortex-Command-Mod-Converter-Engine/")
+    converter_path = converter_dir_path / "Cortex-Command-Mod-Converter-Engine.exe"
+
     output_folder_path = Path("2_converted").resolve()
 
-    extensions = {".zip", ".rte"}
+    entry_paths = list(Path("1_downloads").iterdir())
 
+    index = 0
     for mod_id_folder_path in (
-        entry_path
-        for entry_path in Path("1_downloads").iterdir()
-        if entry_path.is_dir()
+        entry_path for entry_path in entry_paths if entry_path.is_dir()
     ):
-        mod_output_folder_path = output_folder_path / mod_id_folder_path.name
+        # -1 because of .placeholder file
+        print(f"{index + 1}/{len(entry_paths) - 1}")
+        index += 1
 
+        mod_output_folder_path = output_folder_path / mod_id_folder_path.name
         print(mod_output_folder_path)
 
         mod_output_folder_path.mkdir(exist_ok=True)
@@ -24,11 +28,22 @@ def convert_mods():
         for mod_path in (
             entry_path
             for entry_path in mod_id_folder_path.glob("*")
-            if entry_path.suffix in extensions
+            if entry_path.suffix == ".rte"
         ):
-            convert.convert(
-                mod_path, mod_output_folder_path, remove_input_mod_path=True
+            completed_process_instance = subprocess.run(
+                [
+                    converter_path,
+                    mod_path.resolve(),
+                    mod_output_folder_path / mod_path.name,
+                ],
+                cwd=converter_dir_path,
             )
+            # print(completed_process_instance)
+            assert completed_process_instance.returncode == 0
+
+            # TODO: Remove the input folder, if conversion succeeded
+            shutil.rmtree(mod_path)
+
             if len(os.listdir(mod_id_folder_path)) == 0:
                 mod_id_folder_path.rmdir()
 
